8ea62803517daa673beed6387641b8064356b67a
garlife <garlife@gmail.com>
2022-02-15 00:35:54 +0300

    done weaky

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
 export default function App() {
+  const [visibility, setVisibility] = useState(false);
+
+  function handlerVisibility(event){
+    console.log(event.target.checked)
+    return(
+      setVisibility(event.target.checked)
+    )
+    
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <label>Отобразить текст</label>
+      <input type="checkbox" onClick={handlerVisibility}></input>
+      {visibility && <input type="text" defaultValue="Some text"></input>}
     </div>
   );
 }
